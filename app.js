5dc2fe2cba3484d09ca36e1e4f4e7669fcf8a6ac
yaochu1828 <yaochu1828@126.com>
2022-10-18 03:57:13 -0500

    data viz for swing motion

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static('src'));
+
+app.listen(3000, function () {
+  console.log("Server is running on localhost3000");
+});
